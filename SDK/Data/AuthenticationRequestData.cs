e3bb0c054849055cde4597733d0eb07c34a783d7
Marko Malinen <marko.j.malinen@jyu.fi>
2013-09-12 12:11:11 +0300

    Deleted classes with RequestData postfix

--- SDK/Data/AuthenticationRequestData.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace NFleetSDK.Data
-{
-    public class AuthenticationRequestData
-    {
-        public string Username { get; set; }
-        public string Password { get; set; }
-        public string Scope { get; set; }
-    }
-}
