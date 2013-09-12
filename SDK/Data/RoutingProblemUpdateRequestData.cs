4fe5272a396242f81833f39d20762b0e17c94676
Marko Malinen <marko.j.malinen@jyu.fi>
2013-09-12 12:11:11 +0300

    Deleted classes with RequestData postfix

--- SDK/Data/RoutingProblemUpdateRequestData.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace NFleetSDK.Data
-{
-    public class RoutingProblemUpdateRequestData
-    {
-        public int UserId { get; set; }
-        public int ProblemId { get; set; }
-        public string Name { get; set; }
-    }
-}
