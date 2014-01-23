846287070eada47b9a2888a024819b1713cf9d94
Marko Malinen <marko.j.malinen@jyu.fi>
2014-01-23 10:31:07 +0200

    Changed StreetAddress to Street.

--- NFleetSDK/Data/AddressData.cs
+++ NFleetSDK/Data/AddressData.cs
@@ -5,6 +5,6 @@
         public string Country { get; set; }
         public string City { get; set; }
         public string PostalCode { get; set; }
-        public string StreetAddress { get; set; }
+        public string Street { get; set; }
     }
 }
