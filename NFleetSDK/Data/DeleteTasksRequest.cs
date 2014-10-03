015af2bc2a38c3c6e7f3cf16515472f32aedfc7e
Marko Malinen <markojm1986@gmail.com>
2014-10-03 09:22:44 +0300

    Added DeleteTasksRequest

---
+++ NFleetSDK/Data/DeleteTasksRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace NFleet.Data
+{
+    public class DeleteTasksRequest
+    {
+        public int ClientId { get; set; }
+        public int UserId { get; set; }
+        public int ProblemId { get; set; }
+        public int VersionNumber { get; set; }
+    }
+}
