cdba15b57dfa37646b05d6660a0bfdf4dee1ff64
Marko Malinen <markojm1986@gmail.com>
2014-11-10 14:03:30 +0200

    Added user specific limits.

--- NFleetSDK/Data/UserData.cs
+++ NFleetSDK/Data/UserData.cs
@@ -10,6 +10,14 @@ namespace NFleet.Data
         public int VersionNumber { get; set; }
         [DataMember]
         public int Id { get; set; }
+        [DataMember]
+        public int ProblemLimit { get; set; }
+        [DataMember]
+        public int VehicleLimit { get; set; }
+        [DataMember]
+        public int TaskLimit { get; set; }
+        [DataMember]
+        public int OptimizationQueueLimit { get; set; }
 
         #region Implementation of IResponseData
         [DataMember]
