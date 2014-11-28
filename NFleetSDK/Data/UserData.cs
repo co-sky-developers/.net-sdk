2c1231a1d54b36b4e928f1d1543a01a0a6476e77
Antoine Kalmbach <antoine.kalmbach@nfleet.fi>
2014-11-28 15:10:32 +0200

    Add OptimizationCount to UserData.

--- NFleetSDK/Data/UserData.cs
+++ NFleetSDK/Data/UserData.cs
@@ -18,6 +18,8 @@ namespace NFleet.Data
         public int TaskLimit { get; set; }
         [DataMember]
         public int OptimizationQueueLimit { get; set; }
+        [DataMember]
+        public int OptimizationCount { get; set; }
 
         #region Implementation of IResponseData
         [DataMember]
