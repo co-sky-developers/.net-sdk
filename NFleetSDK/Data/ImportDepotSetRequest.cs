a9e1d56906e06c4560b518e0deafc2901f14bfb7
Jukka Kemppainen <jukka.kemppainen@nfleet.fi>
2014-12-17 11:23:10 +0200

    Mr copy-paste strikes again.

--- NFleetSDK/Data/ImportDepotSetRequest.cs
+++ NFleetSDK/Data/ImportDepotSetRequest.cs
@@ -4,10 +4,10 @@ namespace NFleet.Data
 {
     public class ImportDepotSetRequest
     {
-        public List<VehicleUpdateRequest> Items { get; set; }
+        public List<UpdateDepotRequest> Items { get; set; }
         public ImportDepotSetRequest()
         {
-            Items = new List<VehicleUpdateRequest>();
+            Items = new List<UpdateDepotRequest>();
         }
     }
 }
