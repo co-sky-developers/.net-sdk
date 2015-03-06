e1e2a31f2febd98d6be541e19f57d33865b80283
Marko Malinen <marko.malinen@nfleet.fi>
2015-03-06 09:26:22 +0200

    Remove unused using statements

--- NFleetSDK.Example/Program.cs
+++ NFleetSDK.Example/Program.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
-using System.Diagnostics;
 using System.IO;
-using System.Linq;
 using System.Threading;
 using NFleet.Data;
 
