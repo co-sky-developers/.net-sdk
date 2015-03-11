9575efb71ea0ebd9fea046a33ce48669052c44a4
Marko Malinen <marko.malinen@nfleet.fi>
2015-03-11 10:43:30 +0200

    Add properties into UpdateDepotRequest

--- NFleetSDK/Data/UpdateDepotRequest.cs
+++ NFleetSDK/Data/UpdateDepotRequest.cs
@@ -1,6 +1,25 @@
-﻿namespace NFleet.Data
+﻿using System.Collections.Generic;
+
+namespace NFleet.Data
 {
-    public class UpdateDepotRequest : CreateDepotRequest
+    public class UpdateDepotRequest
     {
+        public int DepotId { get; set; }
+
+        public string Name { get; set; }
+        public string Info1 { get; set; }
+        public List<CapacityData> Capacities { get; set; }
+        public LocationData Location { get; set; }
+        public string Type { get; set; }
+        public string DataSource { get; set; }
+        public int VersionNumber { get; set; }
+
+        public string MimeType { get; set; }
+        public string MimeVersion { get; set; }
+
+        public UpdateDepotRequest()
+        {
+            Capacities = new List<CapacityData>();
+        }
     }
 }
