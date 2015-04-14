d51da83511c3aef5af07279a67dcca66364bcd11
Marko Malinen <marko.malinen@nfleet.fi>
2015-04-14 12:45:34 +0300

    Add RoutingProblemSummaryData and SummaryData DTOs

---
+++ NFleetSDK/Data/RoutingProblemSummaryData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NFleet.Data
+{
+    public class RoutingProblemSummaryData : IResponseData, IVersioned
+    {
+        public static string MIMEType = "application/vnd.jyu.nfleet.problemsummary";
+        public static string MIMEVersion = "2.2";
+        public int VersionNumber { get; set; }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Modified { get; set; }
+        public string State { get; set; }
+        public int Progress { get; set; }
+        public RoutingProblemSettingsData Settings { get; set; }
+        public SummaryData Summary { get; set; }
+        public List<Link> Meta { get; set; }
+    }
+}
---
+++ NFleetSDK/Data/SummaryData.cs
@@ -0,0 +1,13 @@
+﻿
+namespace NFleet.Data
+{
+    public class SummaryData
+    {
+        public double TravelDistanceSum { get; set; }
+        public double WorkingTimeSum { get; set; }
+        public int TotalTaskCount { get; set; }
+        public int PlannedTaskCount { get; set; }
+        public int TotalVehicleCount { get; set; }
+        public int UsedVehicleCount { get; set; }
+    }
+}
